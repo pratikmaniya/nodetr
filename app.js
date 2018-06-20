3a42239e9a790d3fab55fb5c6f742465238a095f
pratikmaniya <33822981+pratikmaniya@users.noreply.github.com>
2018-06-20 15:50:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const session = require('express-session');
 const passport = require('passport');
 const config = require('./config/database');
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 mongoose.connect(config.database);
 let db = mongoose.connection;
