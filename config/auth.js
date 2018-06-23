ebe84ad4859a3264561cd8743988d207878192c6
pratikmaniya <pratikmaniya244@gmail.com>
2018-06-23 20:26:00 +0530

    b

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
     'facebookAuth' : {
         'clientId' : '251935238901580',
         'clientSecret' : '25bdc2c1c6bf13f2f7e3fe1054360f56',
-        'callbackURL' : 'http://localhost:8080/auth/facebook/callback'
+        'callbackURL' : 'https://modasa.herokuapp.com/auth/facebook/callback'
     }
 }
