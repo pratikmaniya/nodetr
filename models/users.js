fb8164efe4a85c9a5ad6511dbc51d79fef678838
pratikmaniya <pratikmaniya244@gmail.com>
2018-06-23 21:29:38 +0530

    d

--- models/users.js
+++ models/users.js
@@ -1,21 +1,29 @@
 let mongoose = require('mongoose');
 
 let userSchema = mongoose.Schema({
-    name:{
-        type: String,
-        required: true
+    local:{
+        name:{
+            type: String,
+            required: true
+        },
+        email:{
+            type: String,
+            required: true
+        },
+        username:{
+            type: String,
+            required: true
+        },
+        password:{
+            type: String,
+            required: true
+        }
     },
-    email:{
-        type: String,
-        required: true
-    },
-    username:{
-        type: String,
-        required: true
-    },
-    password:{
-        type: String,
-        required: true
+    facebook:{
+        id: String,
+        token: String,
+        email: String,
+        name: String
     }
 });
 
