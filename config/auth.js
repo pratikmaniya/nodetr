27359e0f9d671fbf868af62f9a96409b16bdc8ce
pratikmaniya <pratikmaniya@gmail.com>
2018-06-26 00:05:48 +0530

    eidted

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
     'googleAuth' : {
         'clientId' : '592734537574-q2nnderds81nee7qp7cf18dph95gt8m8.apps.googleusercontent.com',
         'clientSecret' : 'Thj-5kU4WwvUiWZcNTP6UD7x',
-        'callbackURL' : 'http://localhost:8080/users/auth/google/callback'
+        'callbackURL' : 'http://modasa.herokuapp.com/users/auth/google/callback'
     }
 }
