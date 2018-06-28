e4f2d4a70adc013e7da1f2185c09da28a571ea79
pratikmaniya <pratikmaniya@gmail.com>
2018-06-28 22:07:13 +0530

    link unlink

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
     'googleAuth' : {
         'clientId' : '592734537574-q2nnderds81nee7qp7cf18dph95gt8m8.apps.googleusercontent.com',
         'clientSecret' : 'Thj-5kU4WwvUiWZcNTP6UD7x',
-        'callbackURL' : 'http://localhost:8080/users/auth/google/callback'
+        'callbackURL' : 'https://modasa.herokuapp.com/users/auth/google/callback'
     },
     'facebookAuth':{
         'clientId':'2266591483568331',
