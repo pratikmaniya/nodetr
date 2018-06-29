1573ee7a012b709dd3b63e55b7cb2ebe709e0c8e
pratikmaniya <pratikmaniya@gmail.com>
2018-06-29 15:14:26 +0530

    link unlink

--- config/auth.js
+++ config/auth.js
@@ -2,21 +2,21 @@ module.exports = {
     'googleAuth' : {
         'clientId' : '592734537574-q2nnderds81nee7qp7cf18dph95gt8m8.apps.googleusercontent.com',
         'clientSecret' : 'Thj-5kU4WwvUiWZcNTP6UD7x',
-        'callbackURL' : 'http://localhost:8080/users/auth/google/callback'
+        'callbackURL' : 'https://modasa.herokuapp.com/users/auth/google/callback'
     },
     'facebookAuth':{
-        'clientId':'2266591483568331',
-        'clientSecret':'7eb81cd1754e2f19c84c6c8b0eee24cc',
-        'callbackURL':'https://modasa.herokuapp.com/users/auth/facebook/callback'
+        'clientId' : '2266591483568331',
+        'clientSecret' : '7eb81cd1754e2f19c84c6c8b0eee24cc',
+        'callbackURL' : 'https://modasa.herokuapp.com/users/auth/facebook/callback'
     },
     'twitterAuth':{
-        'clientId':'Qt1gSeWe7uStk3kYJENpqChrD',
-        'clientSecret':'EkzppWP5hARl9eVfzbYq0TWQRifduRUY2Kpoo53c9TpIqTWI3q',
-        'callbackURL':'https://modasa.herokuapp.com/users/auth/twitter/callback'
+        'clientId' : 'Qt1gSeWe7uStk3kYJENpqChrD',
+        'clientSecret' : 'EkzppWP5hARl9eVfzbYq0TWQRifduRUY2Kpoo53c9TpIqTWI3q',
+        'callbackURL' : 'https://modasa.herokuapp.com/users/auth/twitter/callback'
     },
     'githubAuth':{
-        'clientId':'527353234ea9c38fb139',
-        'clientSecret':'9d0bb9ca774f2a862af14d2f71127a1f5f7fa1a8',
-        'callbackURL':'https://modasa.herokuapp.com/users/auth/github/callback'
+        'clientId' : '527353234ea9c38fb139',
+        'clientSecret' : '9d0bb9ca774f2a862af14d2f71127a1f5f7fa1a8',
+        'callbackURL' : 'https://modasa.herokuapp.com/users/auth/github/callback'
     }
 }
