8e291ae6a5e3c44e26419c13a2926117e7aeeb1a
Pratik Maniya <pratikmmaniya244@gmail.com>
2018-09-17 12:23:06 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -128,7 +128,7 @@ app.get('/auth/twitter/callback', passport.authenticate('twitter', {
     failureRedirect: '/register'
 }));
 
-app.get('/auth/github',passport.authenticate('github', { scope: ['email'] }));
+app.get('/auth/github',passport.authenticate('github'));
 
 app.get('/auth/github/callback', passport.authenticate('github', {
     successRedirect: '/users/profile',
